ff5d82925d877c655f752567c34085c52cbda0d6
Foebry <rain_fabry@hotmail.com>
2022-05-13 21:14:12 +0200

    list ingredients

--- src/components/CocktailDetail.jsx
+++ src/components/CocktailDetail.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { useGetCocktailByIdQuery } from "../data/cocktailApi";
+import { useSelector } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
 
 const CocktailDetail = () => {
-  return <div>CocktailDetail</div>;
+  const { cocktailId } = useSelector((state) => state.cocktailState);
+  const { data, isLoading, isError } = useGetCocktailByIdQuery(cocktailId);
+
+  return (
+    <>
+      {isLoading && "calling bartender"}
+      {isError && "Bartender is unavaillable"}
+      {data && (
+        <div>
+          <h1>{data.drinks.strDrink}</h1>
+          <h2>Ingredients</h2>
+          <ul>
+            {Object.keys(data.drinks[0])
+              .filter((key) => key.includes("strIngredient"))
+              .map((key) => data.drinks[0][key])
+              .filter((ingredient) => ingredient > "")
+              .map((ingredient) => (
+                <li key={nanoid(5)}>{ingredient}</li>
+              ))}
+          </ul>
+          <h2>Instructions</h2>
+          <ul>
+            {data.drinks[0].strInstructions
+              .split(/[,.]/)
+              .filter((instr) => instr > "")
+              .map((instr) => (
+                <li key={nanoid(5)}>{instr.trim()}</li>
+              ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default CocktailDetail;
