4b87f7a0292b23ed7e1cb3533a90845394658e4d
Foebry <rain_fabry@hotmail.com>
2022-05-13 17:55:48 +0200

    fetch new cocktails on submit

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setSearchValue } from "../data/cocktailSlicer";
 
 const FormInput = () => {
-  return <div>FormInput</div>;
+  const [value, setValue] = useState("");
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(setSearchValue(value));
+    setValue("");
+  };
+
+  const onChange = (e) => setValue(e.target.value);
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" value={value} onChange={onChange} />
+      <input type="submit" value="search" />
+    </form>
+  );
 };
 
 export default FormInput;
